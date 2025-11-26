b501a0c2f672b3434fa7d675cfd640606c39956f
Kraft <Katharina.Kraft@gesis.org>
2025-11-26 14:25:20 +0100

    Add animal_sounds function and documentation

---
+++ R/animal_sounds.R
@@ -0,0 +1,18 @@
+#' Prints the Sound an Animal Makes
+#'
+#' Prints the sound that an animal makes.
+#'
+#' @param animal A character vector of length 1
+#' @param sound A character vector of length 1
+#'
+#' @returns A string that combines animal name and sound.
+#' @export
+#'
+#' @examples
+#' animal_sounds("dog", "woof")
+animal_sounds <- function(animal, sound) {
+  stopifnot(is.character(animal) & length(animal) == 1)
+  stopifnot(is.character(sound) & length(sound) == 1)
+
+  paste0("The ", animal, " says ", sound, "!")
+}
